34f07430469f0f92dea83527dcef037e09f40c59
zakaria hadraoui <zikoxo05@gmail.com>
2023-11-05 22:03:45 +0100

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+var hamburger=document.querySelector('.togicon')
+hamburger.onclick=function () {
+    var navbar=document.querySelector('.navbar')
+    navbar.classList.toggle('active')
+    
+    
+}
